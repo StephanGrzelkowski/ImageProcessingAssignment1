31045eba253b212fb3432803ad989fdefac690fb
Joeri Bes <joeri.bes@student.uva.nl>
2017-04-13 22:42:41 +0200

    Assignment 5 reupload
    reupload

--- myProjection.m
+++ myProjection.m
@@ -2,15 +2,16 @@ function projection = myProjection(image, x1 , y1 , x2 , y2 , x3 , y3 , x4 , y4 
     projection = zeros(n , m); % allocate new image of correct size
     % calculate projection matrix
     
-    uv = [1, 1; m, 1; 1, n; m, n];
+    uv = [1, 1; 1, m; n, 1; n, m];
     xy = [x1, y1; x2, y2; x3, y3; x4, y4];
     
-    projMatrix = createProjectionMatrix(xy, uv)
+    projMatrix = createProjectionMatrix(xy, uv);
     
     for xIndex = 1: m
         for yIndex = 1: n
             % calculate x and y
             xxyy = projMatrix * [xIndex;yIndex;1];
+            xxyy = xxyy / xxyy(3);
             x = xxyy(1);
             y = xxyy(2); 
             
