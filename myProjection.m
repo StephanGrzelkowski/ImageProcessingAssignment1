64df9d1b7c34651c049a8c565eb46bb3cedee60e
Joeri Bes <joeri.bes@student.uva.nl>
2017-04-13 19:35:57 +0200

    Assignment 5
    Creates a projection matrix however it's not completed yet.

---
+++ myProjection.m
@@ -0,0 +1,21 @@
+function projection = myProjection(image, x1 , y1 , x2 , y2 , x3 , y3 , x4 , y4 , m , n , method, border)
+    projection = zeros(n , m); % allocate new image of correct size
+    % calculate projection matrix
+    
+    uv = [1, 1; m, 1; 1, n; m, n];
+    xy = [x1, y1; x2, y2; x3, y3; x4, y4];
+    
+    projMatrix = createProjectionMatrix(xy, uv)
+    
+    for xIndex = 1: m
+        for yIndex = 1: n
+            % calculate x and y
+            xxyy = projMatrix * [xIndex;yIndex;1];
+            x = xxyy(1);
+            y = xxyy(2); 
+            
+            projection ( yIndex , xIndex ) = ...
+            pixelValue ( image , x , y , method, border);
+        end
+    end
+end
